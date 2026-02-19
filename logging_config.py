2cdab39247676e59bebbe2fcc2a455c9a33f714a
thom899g <153515950+thom899g@users.noreply.github.com>
2026-02-19 18:35:36 +0100

    Automated commit by HiveMind

---
+++ logging_config.py
@@ -0,0 +1,8 @@
+import logging.config
+
+logging_config = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s - %(
